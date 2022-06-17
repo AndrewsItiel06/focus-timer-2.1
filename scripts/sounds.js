64f6629963f1d5f030c894688709249025878047
Andrews Itiel <andrewscarvalho2001@gmail.com>
2022-06-17 00:14:14 -0300

    Update sounds.js

--- scripts/sounds.js
+++ scripts/sounds.js
@@ -2,7 +2,7 @@ import { volControlForest , volControlRaining , volControlCity , volControlFlame
 
 export function soundCards () {
 
-  const forestBg = new Audio("https://github.com/AndrewsItiel06/focus-timer-2.1/blob/main/audios/Floresta.wav?raw=true);
+  const forestBg = new Audio("https://github.com/AndrewsItiel06/focus-timer-2.1/blob/main/audios/Floresta.wav?raw=true");
   const rainingBg = new Audio("https://github.com/AndrewsItiel06/focus-timer-2.1/blob/main/audios/Chuva.wav?raw=true");
   const cityBg = new Audio("https://github.com/AndrewsItiel06/focus-timer-2.1/blob/main/audios/Cafeteria.wav?raw=true");
   const flameBg = new Audio("https://github.com/AndrewsItiel06/focus-timer-2.1/blob/main/audios/Lareira.wav?raw=true");
